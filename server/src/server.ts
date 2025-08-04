0981eaa58e889a6cd056607972388e0891c2c364
Kat <91169645+Pyxbi@users.noreply.github.com>
2025-08-04 21:17:24 +0700

    test

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 app.get('/api', (req, res) => {
   res.json({ message: 'Hello from server!' });
 });

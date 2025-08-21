5a64262861aa7aa264478c03ad95d86608562eb8
Rachel <s3999159@rmit.edu.vn>
2025-08-21 10:30:49 +0700

    fix: bcrypt

--- server/src/controllers/AuthController.ts
+++ server/src/controllers/AuthController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcrypt';
 
 import { VendorModel } from '../models/Vendor';
 import { CustomerModel } from '../models/Customer';

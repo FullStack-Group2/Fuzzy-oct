7952ba0ace4275d3bed3f522b155f0b57fe5f9a5
Pham Nhat Minh <s4019811@rmit.edu.vn>
2025-08-24 11:46:41 +0700

    fix screen problem

---
+++ client/src/features/layout/footer/BackToTopButton.tsx
@@ -0,0 +1,13 @@
+import { RiArrowUpDoubleLine } from "react-icons/ri";
+
+export default function BackToTopButton() {
+  return (
+    <button
+      onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+      className="flex items-center space-x-2 border border-white px-4 py-2 hover:bg-white hover:text-[#1E7A5A] transition"
+    >
+     <RiArrowUpDoubleLine size={24}/> 
+      <span className="text-sm font-medium">BACK TO TOP</span>
+    </button>
+  );
+}

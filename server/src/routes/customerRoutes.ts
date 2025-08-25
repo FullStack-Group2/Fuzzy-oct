f4186a61838e4af98b3939c0f2a367d13a991c18
Kat <91169645+Pyxbi@users.noreply.github.com>
2025-08-25 19:53:03 +0700

    format code

--- server/src/routes/customerRoutes.ts
+++ server/src/routes/customerRoutes.ts
@@ -6,9 +6,28 @@ import { authMiddleware } from '../middleware/authMiddleware';
 const router = Router();
 
 // Routes
-router.get('/cart', authMiddleware, requireCustomer, customerController.getCart);
-router.post('/cart', authMiddleware, requireCustomer, customerController.addToCart);
-router.delete('/cart/:productId', authMiddleware, requireCustomer, customerController.removeItemFromCart);
-router.post('/orders', authMiddleware, requireCustomer, customerController.createOrder);
+router.get(
+  '/cart',
+  authMiddleware,
+  requireCustomer,
+  customerController.getCart,
+);
+router.post(
+  '/cart',
+  authMiddleware,
+  requireCustomer,
+  customerController.addToCart,
+);
+router.delete(
+  '/cart/:productId',
+  authMiddleware,
+  requireCustomer,
+  customerController.removeItemFromCart,
+);
+router.post(
+  '/orders',
+  authMiddleware,
+  requireCustomer,
+  customerController.createOrder,
+);
 export default router;
-

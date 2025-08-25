814f3bdb945d1906a2560f376238b514d0590e3d
Kat <91169645+Pyxbi@users.noreply.github.com>
2025-08-25 19:52:38 +0700

    fix route

--- server/src/routes/customerRoutes.ts
+++ server/src/routes/customerRoutes.ts
@@ -1,46 +1,14 @@
 import { Router } from 'express';
 
 import * as customerController from '../controllers/CustomerController';
-import { authMiddleware } from '../middleware/authMiddleware';
 import { requireCustomer } from '../middleware/roleMiddleware';
-
+import { authMiddleware } from '../middleware/authMiddleware';
 const router = Router();
 
 // Routes
-router.get(
-  '/:id',
-  authMiddleware,
-  requireCustomer,
-  customerController.getCustomerById,
-);
-router.put(
-  '/:id',
-  authMiddleware,
-  requireCustomer,
-  customerController.updateCustomer,
-);
-router.get(
-  '/cart',
-  authMiddleware,
-  requireCustomer,
-  customerController.getCart,
-);
-router.post(
-  '/cart',
-  authMiddleware,
-  requireCustomer,
-  customerController.addToCart,
-);
-router.delete(
-  '/cart/:productId',
-  authMiddleware,
-  requireCustomer,
-  customerController.removeItemFromCart,
-);
-router.post(
-  '/orders',
-  authMiddleware,
-  requireCustomer,
-  customerController.createOrder,
-);
+router.get('/cart', authMiddleware, requireCustomer, customerController.getCart);
+router.post('/cart', authMiddleware, requireCustomer, customerController.addToCart);
+router.delete('/cart/:productId', authMiddleware, requireCustomer, customerController.removeItemFromCart);
+router.post('/orders', authMiddleware, requireCustomer, customerController.createOrder);
 export default router;
+
